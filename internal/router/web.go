51fa89cb684ae73c4690bed057f18dd6fe1e0672
lenye <pyweb@qq.com>
2024-02-28 23:31:19 +0800

    fix: 使用go1.22 ServeMux, 删除github.com/julienschmidt/httprouter

--- internal/router/web.go
+++ internal/router/web.go
@@ -1,4 +1,4 @@
-// Copyright 2023 The aichat Authors. All rights reserved.
+// Copyright 2023-2024 The aichat Authors. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,10 +17,7 @@ package router
 import (
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
-
 	"github.com/lenye/aichat/assets"
-	"github.com/lenye/aichat/internal/handler"
 	"github.com/lenye/aichat/internal/handler/chat"
 	"github.com/lenye/aichat/pkg/project"
 	"github.com/lenye/aichat/pkg/web/alice"
@@ -28,40 +25,28 @@ import (
 	"github.com/lenye/aichat/pkg/web/sse"
 )
 
-func New() *httprouter.Router {
-	r := httprouter.New()
+func New() http.Handler {
+	r := http.NewServeMux()
 	name := "web"
 	stdPipe := alice.New(
 		middleware.AccessLog(name),
 	)
 
-	r.PanicHandler = handler.Panic()
-	r.NotFound = stdPipe.ThenFunc(handler.NotFound)
-
-	r.HandleOPTIONS = true
-	// set a global OPTIONS handler
-	r.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Adjust status code to 204
-		w.WriteHeader(http.StatusNoContent)
-	})
-	r.HandleMethodNotAllowed = true
-	r.MethodNotAllowed = stdPipe.ThenFunc(handler.MethodNotAllowed)
-
 	// static
 	staticFS := assets.StaticFS()
 	fileServer := http.FileServer(http.FS(staticFS))
 	staticChain := alice.New(middleware.ConfigureStaticAssets(project.DevMode()))
-	r.Handler(http.MethodGet, "/favicon.ico", staticChain.Then(fileServer))
-	r.Handler(http.MethodGet, "/static/*filepath", staticChain.Then(http.StripPrefix("/static/", fileServer)))
+	r.Handle("GET /favicon.ico", staticChain.Then(fileServer))
+	r.Handle("GET /static/", staticChain.Then(http.StripPrefix("/static/", fileServer)))
 
 	// sse
-	r.Handler(http.MethodGet, "/chat/sse", stdPipe.ThenFunc(sse.Default().ServeHTTP))
+	r.Handle("GET /chat/sse", stdPipe.ThenFunc(sse.Default().ServeHTTP))
 
 	// tpl
 	tplPipe := stdPipe.Append(middleware.TemplateMap)
-	r.Handler(http.MethodGet, "/chat", tplPipe.ThenFunc(chat.Chat))
-	r.Handler(http.MethodPost, "/chat/sse/msg", tplPipe.ThenFunc(chat.SseMessage))
-	// r.Handler(http.MethodPost, "/chat/msg", tplPipe.ThenFunc(chat.Message))
+	r.Handle("GET /chat", tplPipe.ThenFunc(chat.Chat))
+	r.Handle("POST /chat/sse/msg", tplPipe.ThenFunc(chat.SseMessage))
+	r.Handle("POST /chat/msg", tplPipe.ThenFunc(chat.Message))
 
 	return r
 }
